057c37a9bc089334afd5ecc6b50a24dc9be5c992
Sayali Khot <sayalikhot1801@gmail.com>
2023-06-05 00:40:24 +0530

    updated loading image

--- src/component/Menus.js
+++ src/component/Menus.js
@@ -1,6 +1,7 @@
 import MenuCard from "./MenuCard";
 import { useState } from 'react';
 import '../styles/Menus.css';
+import loading from "../assets/loading.gif"
 function Menus() {
     const [restoList, setRestoList] = useState([]);
     fetch(`https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9715987&lng=77.5945627&page_type=DESKTOP_WEB_LISTING`)
@@ -10,6 +11,11 @@ function Menus() {
         setRestoList(apiData?.data?.cards[2]?.data?.data?.cards)
         // console.log(apiData.data.cards[2].data.data.cards)
     })
+    if (restoList.length===0){
+        return(
+                <img src={loading} alt="loading" id="loading_img"/>
+            )
+    }
 
     return (
         <>
